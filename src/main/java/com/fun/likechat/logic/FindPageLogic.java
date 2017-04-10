8ebed29d751c81843353098deffac0eb62ee7ea0
杨益强 <yqyanghn@126.com>
2017-04-10 16:35:03 +0800

    动态列表全部功能完成，待测试

--- src/main/java/com/fun/likechat/logic/FindPageLogic.java
+++ src/main/java/com/fun/likechat/logic/FindPageLogic.java
@@ -1,56 +1,124 @@
 package com.fun.likechat.logic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fun.likechat.constant.Constant;
 import com.fun.likechat.interceptor.ActionResult;
 import com.fun.likechat.interceptor.BeatContext;
 import com.fun.likechat.interceptor.RequestUtils;
+import com.fun.likechat.persistence.po.ActorDynamicPv;
+import com.fun.likechat.service.ActorDynamicPvService;
+import com.fun.likechat.service.ActorDynamicService;
 import com.fun.likechat.util.LogFactory;
+import com.fun.likechat.vo.ActorDynamicVo;
 
 @Service
 public class FindPageLogic {
-    private static final Logger logger = LogFactory.getInstance().getLogger();
 
-    public ActionResult getFindList(int tag, String stamp) throws Exception {
-        switch (tag) {
-            case 1:
-                return getNewestDynamic(stamp);
-            case 2:
-                return getHotDynamic(stamp);
-            case 3:
-                return getAttentionDynamic(stamp);
+	@Autowired
+	ActorDynamicService actorDynamicService;
+	ActorDynamicPvService actorDynamicPvService;
+	private static final Logger logger = LogFactory.getInstance().getLogger();
 
-        }
-        return ActionResult.fail();
-    }
+	public ActionResult getFindList(int tag, String stamp) throws Exception {
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		int startPage = 0;
+		int count = 0;
+		if (StringUtils.isNotBlank(stamp)) {
+			startPage = Integer.parseInt(stamp);
+		}
+		dataMap.put("startPage", startPage);
+		dataMap.put("pageSize", Constant.PAGEZISE);
+		List<Map<String, Object>> actorDynamics = null;
+		switch (tag) {
+		/*
+		 * 最新动态,按照时间排序
+		 */
+		case 1:
+			actorDynamics = actorDynamicService.getNewestDynamic(dataMap);
+			count = actorDynamicService.newestDynamicCount(dataMap);
+			return toVo(actorDynamics, count, startPage);
 
-    /*
-     * 最新动态
-     */
-    private ActionResult getNewestDynamic(String stamp) {
-        
-        return ActionResult.fail();
-    }
+			/*
+			 * 热门
+			 */
+		case 2:
+			actorDynamics = actorDynamicService.getHotDynamic(dataMap);
+			count = actorDynamicService.hotDynamicCount(dataMap);
+			return toVo(actorDynamics, count, startPage);
+			
+			/*
+			 * 关注
+			 */
+			
+		case 3:
+			// 获取用户信息
+			BeatContext beatContext = RequestUtils.getCurrent();
+			if (beatContext != null && beatContext.getUserid() > 0) {
+				dataMap.put("userId", beatContext.getUserid());
+			} else {
+				logger.debug("没有用户信息");
+			}
+			actorDynamics = actorDynamicService.getAttentionDynamic(dataMap);
+			count = actorDynamicService.attentionDynamicCount(dataMap);
+			return toVo(actorDynamics, count, startPage);
+		}
+		return ActionResult.fail();
+	}
+	private ActionResult toVo(List<Map<String, Object>> actorDynamics, int count, int startPage) {
+		try {
+			if (actorDynamics != null && actorDynamics.size() > 0) {
+				for (Map<String, Object> actorDynamic : actorDynamics) {
+					// 封装返回的vo
+					ActorDynamicVo vo = new ActorDynamicVo();
+					if (actorDynamic.get("content") != null) {
+						vo.setContent(actorDynamic.get("content").toString());
+					}
+					if (actorDynamic.get("icon") != null) {
+						vo.setImgUrl(actorDynamic.get("icon").toString());
+					}
+					if (actorDynamic.get("nickname") != null) {
+						vo.setNickname(actorDynamic.get("nickname").toString());
+					}
+					if (actorDynamic.get("signature") != null) {
+						vo.setSignature(actorDynamic.get("signature").toString());
+					}
+					vo.setUpdateTime(actorDynamic.get("update_time").toString());
+					List<String> dynamicUrl = new ArrayList<String>();
+					// 获取到主播动态资源，如图片、语音、视频
+					ActorDynamicPv adpv = new ActorDynamicPv();
+					adpv.setDynamicId(Integer.parseInt(actorDynamic.get("id").toString()));
+					List<ActorDynamicPv> actorDynamicPvs = actorDynamicPvService.getListByPo(adpv);
+					if (actorDynamicPvs != null && actorDynamicPvs.size() > 0) {
+						for (ActorDynamicPv actorDynamicPv : actorDynamicPvs) {
+							dynamicUrl.add(actorDynamicPv.getSavePath());
+							vo.setDynamicType(actorDynamicPv.getType());
+						}
+					}
+					vo.setDynamicUrl(dynamicUrl);
+					return ActionResult.success(vo, startPage + Constant.PAGEZISE,
+							isNextPage(Constant.PAGEZISE, count, startPage));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ActionResult.fail();
+	}
 
-    /*
-     * 热门
-     */
-    private ActionResult getHotDynamic(String stamp) {
-        return ActionResult.fail();
-    }
-
-    /*
-     * 关注
-     */
-    private ActionResult getAttentionDynamic(String stamp) {
-        //获取用户信息
-        BeatContext beatContext= RequestUtils.getCurrent();
-        if(beatContext!=null && beatContext.getUserid()>0){
-            
-        }else {
-            logger.debug("没有用户信息");
-        }
-        return ActionResult.fail();
-    }
+	public boolean isNextPage(int pageSize, int count, int start) throws Exception {
+		// start,当前页的起始位
+		if ((pageSize + start) < count) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/main/java/com/fun/likechat/service/ActorDynamicServiceImpl.java
+++ src/main/java/com/fun/likechat/service/ActorDynamicServiceImpl.java
@@ -4,7 +4,6 @@ package com.fun.likechat.service;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -51,6 +50,36 @@ public class ActorDynamicServiceImpl implements ActorDynamicService {
 	@Override
 	public int count(Map<String, Object> condition) throws SQLException {
 		return actorDynamicMapper.count(condition);
+	}
+
+	@Override
+	public List<Map<String, Object>> getNewestDynamic(Map<String, Object> condition) throws SQLException {
+		return actorDynamicMapper.getNewestDynamic(condition);
+	}
+
+	@Override
+	public List<Map<String, Object>> getHotDynamic(Map<String, Object> condition) throws SQLException {
+		return actorDynamicMapper.getHotDynamic(condition);
+	}
+
+	@Override
+	public List<Map<String, Object>> getAttentionDynamic(Map<String, Object> condition) throws SQLException {
+		return actorDynamicMapper.getAttentionDynamic(condition);
+	}
+
+	@Override
+	public int hotDynamicCount(Map<String, Object> dataMap) throws SQLException {
+		return actorDynamicMapper.hotDynamicCount(dataMap);
+	}
+
+	@Override
+	public int attentionDynamicCount(Map<String, Object> dataMap) throws SQLException {
+		return actorDynamicMapper.attentionDynamicCount(dataMap);
+	}
+
+	@Override
+	public int newestDynamicCount(Map<String, Object> dataMap) throws SQLException {
+		return actorDynamicMapper.newestDynamicCount(dataMap);
 	}
 	
 }
