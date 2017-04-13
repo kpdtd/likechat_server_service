ef141a0bac1f3633200c53b53afb415fbee76715
suntao <suntao@tigerjoys.com>
2017-04-13 10:46:47 +0800

    no message

--- src/main/java/com/fun/likechat/controller/MessagePageController.java
+++ src/main/java/com/fun/likechat/controller/MessagePageController.java
@@ -1,27 +1,27 @@
-//package com.fun.likechat.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import com.alibaba.fastjson.JSONObject;
-//import com.fun.likechat.interceptor.ActionResult;
-//import com.fun.likechat.util.JsonHelper;
-//
-///**
-// * 发现栏目
-// */
-//@Controller
-//@RequestMapping(value = "/message")
-//public class MessagePageController extends BaseController {
-//
-//	/**
-//	 * 获取动态列表：
-//	 * @param
-//	 * @return
-//	 */
+package com.fun.likechat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fun.likechat.interceptor.ActionResult;
+import com.fun.likechat.util.JsonHelper;
+
+/**
+ * 消息栏目
+ */
+@Controller
+@RequestMapping(value = "/message")
+public class MessagePageController extends BaseController {
+
+	/**
+	 * 获取动态列表：
+	 * @param
+	 * @return
+	 */
 //	@RequestMapping(value = "getFindList", method = RequestMethod.POST)
 //	public @ResponseBody ActionResult getFindList(@RequestBody String body) {
 //		try {
@@ -36,25 +36,25 @@
 //		}
 //		return ActionResult.fail();
 //	}
-//	
-//	/**
-//	 * 增加动态
-//	 * @param
-//	 * @return
-//	 */
-//	@RequestMapping(value = "addDynamic", method = RequestMethod.POST)
-//	public @ResponseBody ActionResult addDynamic(@RequestBody String body) {
-//		try {
+	
+	/**
+	 * 通话记录--这个表要和sdkdemo结合
+	 * @param
+	 * @return
+	 */
+	@RequestMapping(value = "callRecord", method = RequestMethod.POST)
+	public @ResponseBody ActionResult callRecord(@RequestBody String body) {
+		try {
 //			JSONObject json = JsonHelper.toJsonObject(body);
 //			int actorId = json.getIntValue("condition");
 //			//根据条件获取动态列表
-//
-//			return new ActionResult();
-//		}
-//		catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//		return ActionResult.fail();
-//	}
-//
-//}
+
+			return new ActionResult();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return ActionResult.fail();
+	}
+
+}
--- src/test/java/com/fun/likechat/junit/control/HomePageControllerTest.java
+++ src/test/java/com/fun/likechat/junit/control/HomePageControllerTest.java
@@ -1,16 +1,52 @@
 package com.fun.likechat.junit.control;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+
+import com.fun.likechat.util.HttpClient;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class HomePageControllerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void a_getHomePageContent() throws IOException {
+		String result = HttpClient.postRequest("http://localhost:8080/likechat_server_service/home/getHomePageContent", "");
+		System.out.println("result:" + result);
 	}
+	
+	@Test
+	public void b_getActorListByTag() throws IOException {
+		String json = "{\"identifying\":\"1\"}";
+		String result = HttpClient.postRequest("http://localhost:8080/likechat_server_service/home/getActorListByTag", json);
+		System.out.println("result:" + result);
+	}
+	
+	@Test
+	public void c_getActorPage() throws IOException {
+		String json = "{\"identifying\":1}";
+		String result = HttpClient.postRequest("http://localhost:8080/likechat_server_service/home/getActorPage", json);
+		System.out.println("result:" + result);
+	}
+	
+	@Test
+	public void d_addAttention() throws IOException {
+		String json = "{\"userId\":1,\"actorId\":1}";
+		String result = HttpClient.postRequest("http://localhost:8080/likechat_server_service/home/addAttention", json);
+		System.out.println("result:" + result);
+	}
+	
+	@Test
+	public void e_cancelAttention() throws IOException {
+		String json = "{\"userId\":1,\"actorId\":1}";
+		String result = HttpClient.postRequest("http://localhost:8080/likechat_server_service/home/cancelAttention", json);
+		System.out.println("result:" + result);
+	}
+	
+
 
 }
